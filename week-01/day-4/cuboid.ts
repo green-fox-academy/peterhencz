9e28cfcf6f342afe3d4c68435acb0406337e5d06
Peter Hencz <peter.hencz@gmail.com>
2018-02-15 17:40:51 +0100

    cuboid

---
+++ week-01/day-4/cuboid.ts
@@ -0,0 +1,10 @@
+'use strict'
+
+let a: number = 5.0;
+let b: number = 6.0;
+let c: number = 3.0;
+let surface: number = (((a * b) + (b * c) + (a * c)) * 2);
+        console.log("Surface Area: " + surface);
+let volume: number = (a * b * c);
+        console.log("Volume Area: " + volume);
+
