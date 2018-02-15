cb97f63f4006a022a5b3466ca1f8c6ae537cda72
Peter Hencz <peter.hencz@gmail.com>
2018-02-15 14:17:01 +0100

    ts

---
+++ week-01/day-4/bmi.ts
@@ -0,0 +1,22 @@
+'use strict';
+
+
+let massInKg: number = 81.2;
+let heightInM: number = 1.78;
+let bmi: number = massInKg / (heightInM * heightInM);
+
+console.log('Your BMI is: ' + bmi.toFixed(0)); 
+
+
+
+// other version with math.round
+
+/* 
+
+let massInKg: number = 81.2;
+let heightInM: number = 1.78;
+let bmi: number = Math.round(massInKg / (heightInM * heightInM));
+
+console.log('Your BMI is: ' + bmi);
+
+*/
---
+++ week-01/day-4/dbi.ts
@@ -0,0 +1,12 @@
+'use strict'
+
+var myName: string= 'Peter Hencz';
+var myAge: number = (31);
+var myHeight: number = (1.75);
+var isMarried: boolean = (false);
+
+if (isMarried) {
+        console.log('Hey, my name is ' + myName + " and I'm " + myAge + ' years old. I am ' + myHeight + ' tall and married.');  
+} else {
+        console.log('Hey, my name is ' + myName + " and I'm " + myAge + ' years old. I am ' + myHeight + ' tall and not married.');
+}
