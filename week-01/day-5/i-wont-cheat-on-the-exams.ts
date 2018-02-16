233a85aa9598ec191e925d4cf12df2d3ae6a30c3
Peter Hencz <peter.hencz@gmail.com>
2018-02-16 10:47:34 +0100

    i wont cheat

--- week-01/day-5/i-wont-cheat-on-the-exams.ts
+++ week-01/day-5/i-wont-cheat-on-the-exams.ts
@@ -3,6 +3,7 @@
 // Create a program that writes this line 100 times:
 // "I won't cheat on the exam!"
 
+
 for (let i: number = 1; i <=100 ; i++) {
     console.log ('I won\'t cheat on the exam!');
 }
