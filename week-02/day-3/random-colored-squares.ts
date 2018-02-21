26bf49ecee2ecc4249d24b4a8478f11ebf42a8d9
Peter Hencz <peter.hencz@gmail.com>
2018-02-21 19:05:03 +0100

    random colored sqrs - practice

--- week-02/day-3/random-colored-squares.ts
+++ week-02/day-3/random-colored-squares.ts
@@ -4,6 +4,8 @@
 const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d');
 
+// ezt meg be kell rakni loopba, hogy szexibb legyen
+
 function randomColorSqr() {
     ctx.fillStyle = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
     ctx.fillRect((canvas.scrollWidth/3), 200, 100, 100);
