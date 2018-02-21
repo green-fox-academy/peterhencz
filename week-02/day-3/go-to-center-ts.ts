b084ff5352d5fa58747055dca941a0b594090362
Peter Hencz <peter.hencz@gmail.com>
2018-02-21 11:47:22 +0100

    go-to-center

---
+++ week-02/day-3/go-to-center-ts.ts
@@ -0,0 +1,42 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+// DO NOT TOUCH THE CODE ABOVE THIS LINE
+// Create a line drawing function that takes 2 parameters:
+// The x and y coordinates of the line's starting point
+// and draws a line from that point to the center of the canvas
+// Draw 3 lines with that function. Use loop for that.
+
+let x: number = 0;
+let y: number = 0;
+
+/*
+ctx.strokeStyle = "green";
+ctx.beginPath();
+ctx.moveTo(x, y);
+ctx.stroke();
+*/
+
+function goToCenter(x : number, y : number) { 
+    ctx.beginPath();
+    ctx.strokeStyle = "pink";
+    ctx.moveTo(x, y);
+    ctx.lineTo(300, 200);
+    ctx.stroke();
+
+}
+
+for (let i: number = 1; i <= 3; i++) {
+    x = parseInt(prompt('Mennyi az x?'));
+    y = parseInt(prompt('Mennyi az y?'));
+    goToCenter(x, y);
+
+}
+/*
+
+goToCenter(parseInt(myX),10);
+goToCenter(30,120);
+goToCenter(420, 342);
+*/
