687cb760a339b6c1aade3b117646ad7e02672e15
Peter Hencz <peter.hencz@gmail.com>
2018-02-22 20:04:38 +0100

    stary night with moon craters

--- week-02/day-4/stary_try.ts
+++ week-02/day-4/stary_try.ts
@@ -11,9 +11,6 @@ const ctx = canvas.getContext('2d');
 //  - The stars should have random color (some shade of grey)
 
 
-let x: number = 0;
-let y: number = 0;
-
 
 // sky
 
@@ -23,44 +20,81 @@ let grds = ctx.createLinearGradient(canvas.scrollWidth/2, 0, canvas.scrollWidth/
     ctx.fillStyle = grds;
     ctx.fillRect(0, 0, canvas.scrollWidth, canvas.scrollHeight);
 
-            
-// stars attr
-
-function drawStars(x,y) {  
-    ctx.fillStyle = ctx.fillStyle = "rgba("+ (237) + "," + (199) + "," + (11)+ "," + (i * 0.025) + ")";
-    ctx.fillRect(x, y, (0.012 * i), (0.012 * i));
-
-}
-
+// stars
 
 for (let i = 1; i<=135; i++) {
     let x: number = (Math.random() * (canvas.scrollWidth - 5));  
     let y: number = (Math.random() * (canvas.scrollHeight - 5));
+    function drawStars(x,y) {  
+        ctx.fillStyle = ctx.fillStyle = "rgba("+ (237) + "," + (199) + "," + (11)+ "," + (i * 0.025) + ")";
+        ctx.fillRect(x, y, (0.012 * i), (0.012 * i));
+        }
     drawStars(x,y);
 }
 
 // moon
 
+
 function drawMoon() {  
     ctx.beginPath();
-    ctx.arc(405, 105, 40, 0, Math.PI * 2, true); // Outer circle
+    ctx.arc(400, 130, 40, 0, Math.PI * 2, true); // Outer circle
     let grd = ctx.createLinearGradient(0, 0, canvas.scrollWidth/2, canvas.scrollHeight);
-    grd.addColorStop(0, "#7c929e");
-    grd.addColorStop(1, "#091a23");
+    grd.addColorStop(0.6, "#596d77");
+    grd.addColorStop(0.7, "#242b30");
+    ctx.fillStyle = grd;
+    ctx.fill();
+ 
+}
+
+function drawMoonCrater() {  
+    ctx.beginPath();
+    ctx.arc(405, 108, 12, 0, Math.PI * 2, true); // Outer circle
+    let grd = ctx.createLinearGradient(canvas.scrollWidth/4.5, 0, canvas.scrollWidth/2, canvas.scrollHeight);
+    grd.addColorStop(0.44, "#242b30");
+    grd.addColorStop(0.53, "#596d77");
+    ctx.fillStyle = grd;
+    ctx.fill();
+ 
+}
+
+function drawMoonCrater2() {  
+    ctx.beginPath();
+    ctx.arc(412, 150, 10, 0, Math.PI * 2, true); // Outer circle
+    let grd = ctx.createLinearGradient(canvas.scrollWidth/2.6, 0, canvas.scrollWidth, canvas.scrollHeight);
+    grd.addColorStop(0.42, "#242b30");
+    grd.addColorStop(0.52, "#596d77");
+    ctx.fillStyle = grd;
+    ctx.fill();
+ 
+}
+
+function drawMoonCrater3() {  
+    ctx.beginPath();
+    ctx.arc(380, 140, 8, 0, Math.PI * 2, true); // Outer circle
+    let grd = ctx.createLinearGradient(canvas.scrollWidth/2.6, 0, canvas.scrollWidth, canvas.scrollHeight);
+    grd.addColorStop(0.34, "#242b30");
+    grd.addColorStop(0.50, "#596d77");
     ctx.fillStyle = grd;
     ctx.fill();
  
 }
 
 drawMoon();
+drawMoonCrater();
+drawMoonCrater2();
+drawMoonCrater3();
+
 
 // moon gradient
 
-let grdmg = ctx.createRadialGradient(405, 100, 140, 405, 100, 0);
-grdmg.addColorStop(0, 'rgba(40, 40, 40, 0)');
-grdmg.addColorStop(1, 'rgba(124, 146, 158, 0.4)');
-ctx.fillStyle = grdmg;
-ctx.fillRect(0, 0, canvas.scrollWidth, canvas.scrollHeight);
+function drawMoonGrd {  
+    let grdmg = ctx.createRadialGradient(405, 100, 140, 405, 100, 0);
+    grdmg.addColorStop(0, 'rgba(40, 40, 40, 0)');
+    grdmg.addColorStop(1, 'rgba(124, 146, 158, 0.4)');
+    ctx.fillStyle = grdmg;
+    ctx.fillRect(0, 0, canvas.scrollWidth, canvas.scrollHeight);
 
 
+}
 
+drawMoonGrd();
