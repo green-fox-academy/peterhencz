46004dc1368192bbba3567d7e74c177f6eb86bc1
Peter Hencz <peter.hencz@gmail.com>
2018-02-22 01:39:49 +0100

    home practice with random color

--- week-02/day-3/random-colored-squares.ts
+++ week-02/day-3/random-colored-squares.ts
@@ -4,17 +4,17 @@
 const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d');
 
-// ezt meg be kell rakni loopba, hogy szexibb legyen
+let a = prompt('Think a number between 1 and 10: ');
+let b = prompt('Nice, now let\'s see another between: 100 and 200');
 
-function randomColorSqr() {
-    ctx.fillStyle = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
-    ctx.fillRect((canvas.scrollWidth/3), 200, 100, 100);
-    ctx.fillStyle = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
-    ctx.fillRect((canvas.scrollWidth/2), 200, 100, 100);
-    ctx.fillStyle = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
-    ctx.fillRect((canvas.scrollWidth/3), 100, 100, 100);
-    ctx.fillStyle = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
-    ctx.fillRect((canvas.scrollWidth/2), 100, 100, 100);
+function randomColorSqr {
+    for (let i = 0; i<200; i++) {
+        ctx.fillStyle = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+        ctx.fillRect((canvas.scrollWidth/2-(a*i)), (100 + (i*a)), (10 / i), (i*b));
+        ctx.fillStyle = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+        ctx.fillRect((canvas.scrollWidth-(12*i)), b - (a * i), (120/a), (b/i));
+    
+    }
 }
-
 randomColorSqr();
+
