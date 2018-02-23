a949189d144d6e7506b314da91297675905eb0ad
Peter Hencz <peter.hencz@gmail.com>
2018-02-23 13:45:12 +0100

    stary night update with milky way

--- week-02/day-4/stary_try.ts
+++ week-02/day-4/stary_try.ts
@@ -14,12 +14,13 @@ const ctx = canvas.getContext('2d');
 /*  tasks for the weekend:
                         
     - fix the rng parts
+    - change colors to hsla
     - write one loop for the craters ( - crater positions inside the moon area
                                        - crater sizes between ? and ?  
                                        - gradient colorstop positions  )
                                        
     - set canvas-free values with canvas.scrollWidth & canvas.scrollHeight
-    - milky way oriented stars
+    - milky way oriented stars (left & right side?)
     - add clouds, planes and angels
 
     */
@@ -27,35 +28,57 @@ const ctx = canvas.getContext('2d');
 // sky
 
 let grds = ctx.createLinearGradient(0, canvas.scrollHeight*0.4, canvas.scrollWidth, canvas.scrollHeight*0.8);
-    grds.addColorStop(0,"#161e23");
-    grds.addColorStop(0.3,"#253742");
-    grds.addColorStop(0.6,"#161e23");
+    grds.addColorStop(0, 'hsla(203, 33%, 11%, 1)');
+    grds.addColorStop(0.3, 'hsla(203, 18%, 20%, 1)');
+    grds.addColorStop(0.6, 'hsla(203, 33%, 11%, 1)');
     ctx.fillStyle = grds;
     ctx.fillRect(0, 0, canvas.scrollWidth, canvas.scrollHeight);
 
-// stars - normal version
 
-for (let i = 1; i<=1000; i++) {
-    let x: number = ((Math.random() * (canvas.scrollWidth)));  
+// aron fele csillageloszlas - right side
+
+for (let i = 1; i<= 1200; i++) {
+    let x: number = (canvas.scrollWidth)/0.002/(0.4*(i*5))+140;
     let y: number = ((Math.random() * (canvas.scrollHeight)));
-    function drawStars(x,y) {  
-        ctx.fillStyle = ctx.fillStyle = "rgba("+ (237) + "," + (199) + "," + (11)+ "," + (i * 0.001) + ")";
-        ctx.fillRect(x, y, (i/1000), (i/1000));
-        }
+    for (let j: number = 0; j < 100; j++) {
+        if (j * 4 < y && ((j + 1) * 4) > y) {     
+        x -= j * 1.2;
+        function drawStars(x,y) {  
+            ctx.fillStyle = ctx.fillStyle = "rgba("+ (237) + "," + (199) + "," + (11)+ "," + (((i*4) * 0.001)/1.2) + ")";
+            ctx.fillRect(x, y, (i*0.00056), (i*0.00056));
+            }
    drawStars(x,y);
+        }
+    }
+}
+// aron fele csillageloszlas - left side
+
+for (let i = 1; i<=1200; i++) {
+    let x: number = (canvas.scrollWidth)*3/(6/(i*0.00032))+150;
+    let y: number = ((Math.random() * (canvas.scrollHeight)));
+    for (let j: number = 0; j < 100; j++) {
+        if (j * 4 < y && ((j + 1) * 4) > y) {     
+        x -= j * 1.2;
+        function drawStars(x,y) {  
+            ctx.fillStyle = ctx.fillStyle = "rgba("+ (237) + "," + (199) + "," + (11)+ "," + (((i*4) * 0.001)/1.2) + ")";
+            ctx.fillRect(x, y, (i*0.00056), (i*0.00056));
+            }
+         drawStars(x,y);
+        }
+    }
 }
 
 
+    // stars - third layer (for full canvas)
 
-// sun gradient
-
-function sunBGrd() {  
-    let grdsnbgrd = ctx.createRadialGradient(220, 100, 20, 220, 100, 0);
-    grdsnbgrd.addColorStop(0.8, 'rgba(40, 40, 40, 0)');
-    grdsnbgrd.addColorStop(1, 'rgba(178, 148, 1, 1)');
-    ctx.fillStyle = grdsnbgrd;
-    ctx.fillRect(0, 0, canvas.scrollWidth, canvas.scrollHeight);
-
+for (let i = 1; i<=1000; i++) {
+    let x: number = ((Math.random() * (canvas.scrollWidth)));
+    let y: number = ((Math.random() * (canvas.scrollHeight)));
+    function drawStars(x,y) {  
+        ctx.fillStyle = ctx.fillStyle = 'hsla('+ (50) + ',' + (91) + ',' + (49)+ ',' + (i * 0.00006) + ')';
+        ctx.fillRect(x, y, (i*0.00076), (i*0.00076));
+        }
+     drawStars(x,y);
 }
 
 
@@ -63,8 +86,8 @@ function sunBGrd() {
 
 function drawMoonGrd() {  
     let grdmg = ctx.createRadialGradient(390, 140, 100, 370, 140, 0);
-    grdmg.addColorStop(0, 'rgba(40, 40, 40, 0)');
-    grdmg.addColorStop(1, 'rgba(73, 102, 117, 0.5)');
+    grdmg.addColorStop(0, 'hsla(0, 0%, 16%, 0)');
+    grdmg.addColorStop(1, 'hsla(200, 23%, 37%, 0.5)');
     ctx.fillStyle = grdmg;
     ctx.fillRect(0, 0, canvas.scrollWidth, canvas.scrollHeight);
 
@@ -74,8 +97,8 @@ function drawMoon() {
     ctx.beginPath();
     ctx.arc(400, 130, 41, 0, Math.PI * 2, true); 
     let grd = ctx.createLinearGradient(canvas.scrollHeight/4, canvas.scrollWidth, canvas.scrollWidth*1.5, canvas.scrollHeight/2);
-    grd.addColorStop(0.51, "#496675");
-    grd.addColorStop(0.56, "#212e35");
+    grd.addColorStop(0.5, 'hsla(200, 13%, 37%, 1)');
+    grd.addColorStop(0.55, 'hsla(201, 23%, 17%, 1)');
     ctx.fillStyle = grd;
     ctx.fill();
  
@@ -88,8 +111,8 @@ function drawMoonCrater1() {
     ctx.beginPath();
     ctx.arc(405, 108, 9.6, 0, Math.PI * 2, true);
     let grd = ctx.createLinearGradient(canvas.scrollWidth*1.5, canvas.scrollHeight/2, canvas.scrollWidth/4, canvas.scrollWidth);
-    grd.addColorStop(0.26, "#496675");
-    grd.addColorStop(0.51, "#212e35");
+    grd.addColorStop(0.26, 'hsla(200, 13%, 37%, 1)');
+    grd.addColorStop(0.51, 'hsla(201, 23%, 17%, 1)');
     ctx.fillStyle = grd;
     ctx.fill();
  
@@ -99,8 +122,8 @@ function drawMoonCrater2() {
     ctx.beginPath();
     ctx.arc(412, 150, 6, 0, Math.PI * 2, true); 
     let grd = ctx.createLinearGradient(canvas.scrollWidth*1.5, canvas.scrollHeight/2, canvas.scrollWidth/4, canvas.scrollWidth);
-    grd.addColorStop(0.44, "#496675");
-    grd.addColorStop(0.5, "#212e35");
+    grd.addColorStop(0.44, 'hsla(200, 13%, 37%, 1)');
+    grd.addColorStop(0.5, 'hsla(201, 23%, 17%, 1)');
     ctx.fillStyle = grd;
     ctx.fill();
  
@@ -110,8 +133,8 @@ function drawMoonCrater3() {
     ctx.beginPath();
     ctx.arc(380, 140, 7, 0, Math.PI * 2, true); 
     let grd = ctx.createLinearGradient(canvas.scrollWidth*1.5, canvas.scrollHeight/2, canvas.scrollWidth/4, canvas.scrollWidth);
-    grd.addColorStop(0.5065, "#496675");
-    grd.addColorStop(0.53, "#212e35");
+    grd.addColorStop(0.5065, 'hsla(200, 13%, 37%, 1)');
+    grd.addColorStop(0.53, 'hsla(201, 23%, 17%, 1)');
     ctx.fillStyle = grd;
     ctx.fill();
  
@@ -121,8 +144,8 @@ function drawMoonCrater4() {
     ctx.beginPath();
     ctx.arc(380, 113, 3, 0, Math.PI * 2, true); 
     let grd = ctx.createLinearGradient(canvas.scrollWidth*1.5, canvas.scrollHeight/2, canvas.scrollWidth/4, canvas.scrollWidth);
-    grd.addColorStop(0.48, "#496675");
-    grd.addColorStop(0.51, "#212e35");
+    grd.addColorStop(0.48, 'hsla(200, 13%, 37%, 1)');
+    grd.addColorStop(0.51, 'hsla(201, 23%, 17%, 1)');
     ctx.fillStyle = grd;
     ctx.fill();
  
@@ -132,8 +155,8 @@ function drawMoonCrater5() {
     ctx.beginPath();
     ctx.arc(424, 130, 7, 0, Math.PI * 2, true); 
     let grd = ctx.createLinearGradient(canvas.scrollWidth*1.5, canvas.scrollHeight/2, canvas.scrollWidth/4, canvas.scrollWidth);
-    grd.addColorStop(0.43, "#496675");
-    grd.addColorStop(0.45, "#212e35");
+    grd.addColorStop(0.43, 'hsla(200, 13%, 37%, 1)');
+    grd.addColorStop(0.45, 'hsla(201, 23%, 17%, 1)');
     ctx.fillStyle = grd;
     ctx.fill();
  
