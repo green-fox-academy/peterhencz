8a771c2d38ee114586ca2431d9ebf549e18a2b51
Peter Hencz <peter.hencz@gmail.com>
2018-03-01 14:23:55 +0100

    bunny

---
+++ week-03/day-4/bunny1.ts
@@ -0,0 +1,19 @@
+'use strict'
+
+// We have a number of bunnies and each bunny has two big floppy ears.
+// We want to compute the total number of ears across all the bunnies recursively (without loops or multiplication).
+
+function bunnyEars(i: number) {
+  let ears: number = 0;
+  if (i < 1) {
+    return ears;
+  }
+  else {
+    ears += 2;
+    i -= 1;
+    return ears += bunnyEars(i);
+  }
+}
+console.log(bunnyEars(120));
+
+
