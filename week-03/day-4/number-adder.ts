7d243b3f6bd0ae0fec2cbf68fc57476801f2f36b
Peter Hencz <peter.hencz@gmail.com>
2018-03-01 10:30:10 +0100

    number adder

---
+++ week-03/day-4/number-adder.ts
@@ -0,0 +1,13 @@
+'use strict'
+
+// Write a recursive function that takes one parameter: n and adds numbers from 1 to n.
+
+function countDown(num: number) {
+  if (num = num) {
+    return num;
+  }
+  else {
+    return countDown(num + 1);
+  }
+}
+console.log(countDown(0));
