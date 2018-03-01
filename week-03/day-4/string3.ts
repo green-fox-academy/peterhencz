3e6f1f9f97ec7cf9829772d6827e569a151a5546
Peter Hencz <peter.hencz@gmail.com>
2018-03-01 16:55:21 +0100

    string 3

---
+++ week-03/day-4/string3.ts
@@ -0,0 +1,15 @@
+'use strict'
+
+
+// Given a string, compute recursively a new string where all the
+// adjacent chars are now separated by a '*'.
+
+
+function separatedByAStar(string: string) {
+  if (string.charAt(string.length-2) == '*') {
+    return string;
+  }
+  string = string.match(/.{1}/g).join('*');
+  return separatedByAStar(string);
+}
+console.log(separatedByAStar('xbbxbxbbbbxbbxbbxbbxbxbbxbbbx'))
