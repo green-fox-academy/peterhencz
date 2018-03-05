c2e28d19a7c6cf969f364cabf4c865fa49427767
Peter Hencz <peter.hencz@gmail.com>
2018-03-05 10:23:39 +0100

    count as

---
+++ week-04/day-01/exam-trial-basics-master/countas/CountAs.ts
@@ -0,0 +1,35 @@
+'use strict'
+
+// Create a function that takes a filename as string parameter,
+// counts the occurances of the letter "a", and returns it as a number.
+// If the file does not exist, the function should return 0 and not break.
+
+// example: on the input "afile.txt" the function should return 28 - print this result
+// example: on the input "not-a-file" the function should return 0 - print this result
+
+let a = require('fs');
+
+function countAs(a: number) {
+  let fileContent = a.readFileSync('afile.txt', 'utf-8');
+  let as: number [] = fileContent.split('a');
+  return as.length;
+}
+try {
+  console.log(countAs(a));
+}
+catch (error) {
+  console.log('0');
+}
+
+
+// function printLines() {
+//   let fileContent = fs.readFileSync('my-file.txt', 'utf-8');
+//   let lineNumbers: number[] = fileContent.split('\n');
+//   return lineNumbers.length;
+// }
+// try {
+//   console.log(printLines());
+// }
+// catch (error) {
+//   console.log('0');
+// }
