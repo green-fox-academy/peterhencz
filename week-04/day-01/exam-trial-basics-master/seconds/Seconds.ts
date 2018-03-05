d729f15039db4d1fd5eab0899c36e17bc87014a5
Peter Hencz <peter.hencz@gmail.com>
2018-03-05 10:13:23 +0100

    seconds

---
+++ week-04/day-01/exam-trial-basics-master/seconds/Seconds.ts
@@ -0,0 +1,16 @@
+'use strict'
+
+// Create a function that takes a list as a parameter,
+// and returns a new list with every second element from the orignal list
+// example: [1, 2, 3, 4, 5] should produce [2, 4] - print this result
+
+let list: string [] = ['1', '2', '3', '4', '5', '6', '7'];
+
+function secondFromTheList (l: string[]): any { 
+return list.filter(function(value, index, arr) {
+    return index % 2 == 0;
+  });
+}
+
+console.log(secondFromTheList(list));
+
