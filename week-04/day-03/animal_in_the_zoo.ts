c555e3a2fb03d1bcb643b2497342366486dae16e
Peter Hencz <peter.hencz@gmail.com>
2018-03-08 00:07:51 +0100

    fin

--- week-04/day-03/animal_in_the_zoo.ts
+++ week-04/day-03/animal_in_the_zoo.ts
@@ -9,51 +9,50 @@ abstract class Animal {
  age: number = 0;
 	species: string;
 
-	constructor(name:string) {
+	constructor(name:string, age: number) {
 		this.name = name;
+		this.age = age;
+
 	}
 
-
- // animalSpecies() {}
-
-	// getAge() {}
+getName(): string {
+		return this.name;
+	}
 
 	getName(): string {
 		return this.name;
 	}
 
 	breed() {}
-
 }
 
 class Reptile extends Animal {
-	
-	constructor(name: string, age: number) {
-		super(name);
-		this.age = age;
-	}
 
 	breed(): string {
 		return 'laying eggs';
 	}
+}
 
+class Mammal extends Animal {
+
+	breed(): string {
+		return 'pushing miniature versions out.';
+	}
+}
+
+class Bird extends Animal {
+
+	breed(): string {
+		return 'laying eggs.';
+	}
 }
 
 
-// abstract class Mammal extends Animal {
-
-// }
-
-// abstract class Bird extends Animal {
-
-// }
-
-
 let myReptile = new Reptile("Crocodile", 5);
-// let mammal = new Mammal("Koala");
-// let bird = new Bird("Parrot");
+let mammal = new Mammal("Koala", 15);
+let bird = new Bird("Parrot", 11);
 
-console.log("How do you breed?");
-console.log("A " + myReptile.getName() + " is breeding by " + myReptile.breed() + ".");
-// console.log("A " + mammal.getName() + " is breeding by " + mammal.breed());
-// console.log("A " + bird.getName() + " is breeding by " + bird.breed());
+console.log('\n' + "How do you breed?" + '\n');
+console.log("A " + myReptile.getName() + " is breeding by " + myReptile.breed());
+console.log("A " + mammal.getName() + " is breeding by " + mammal.breed());
+console.log("A " + bird.getName() + " is breeding by " + bird.breed() + '\n');
