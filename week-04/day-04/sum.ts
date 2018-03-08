3f8b82c467b014dc45a4c84cfd8ed669f35a76d5
Peter Hencz <peter.hencz@gmail.com>
2018-03-09 00:23:34 +0100

    cant believe i did it

---
+++ week-04/day-04/sum.ts
@@ -0,0 +1,24 @@
+
+
+'use strict'
+
+
+export class SumClass {
+  private list: number[];
+
+
+  constructor(list: number[]) {
+    this.list = list;
+  }
+
+  sum(): any {
+    if (Array.isArray(this.list)) {
+      return this.list.reduce((a, b) => a + b, 0);
+    }
+    else {
+      return '0'
+    }
+  }
+}
+
+
