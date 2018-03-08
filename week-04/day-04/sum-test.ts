6d8a514157e4a2f7b19506d121f903f415c1797a
Peter Hencz <peter.hencz@gmail.com>
2018-03-09 00:26:23 +0100

    finally done

---
+++ week-04/day-04/sum-test.ts
@@ -0,0 +1,55 @@
+'use strict'
+
+import {test} from 'tape';
+import {SumClass} from './sum';
+
+test('Test empty sum list', t => {
+  let sumTest1 = new SumClass([]);
+
+  let actual = sumTest1.sum();
+  let expected = 0;
+
+   t.equal(actual, expected);
+   t.end();
+});
+
+test('Test sum list with one element', t => {
+  let sumTest2 = new SumClass([418]);
+
+  let actual = sumTest2.sum();
+  let expected = 418;
+
+   t.equal(actual, expected);
+   t.end();
+});
+
+test('Test sum list with elements', t => {
+  let sumTest3 = new SumClass([1, 2, 3, 4, 5]);
+
+  let actual = sumTest3.sum();
+  let expected = 15;
+
+   t.equal(actual, expected);
+   t.end();
+});
+
+test('Test sum list with a null', t => {
+  let sumTest4 = new SumClass(null);
+
+  let actual = sumTest4.sum();
+  let expected = '0';
+
+   t.equal(actual, expected);
+   t.end();
+});
+
+test('Test sum list with a string', t => {
+  let sumTest4 = new SumClass(null);
+
+  let actual = sumTest4.sum();
+  let expected = '0';
+
+   t.equal(actual, expected);
+   t.end();
+});
+
