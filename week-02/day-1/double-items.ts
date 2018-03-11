e9aa7bbc30d5146e9821141f9a77d943f6427d87
Peter Hencz <peter.hencz@gmail.com>
2018-03-12 00:23:12 +0100

    double items

---
+++ week-02/day-1/double-items.ts
@@ -0,0 +1,18 @@
+'use strict'
+
+// -  Create an array variable named `names` with the following content: `['Gin', 'Whiskey', 'Wine', 'Beer']`
+// -  Double all the strings in the array, use a built in array method instead of a loop
+// It should print: ['GinGin', 'WhiskeyWhiskey', 'WineWine', 'BeerBeer']`
+
+let names: string[] = ['Gin', 'Whiskey', 'Wine', 'Beer'];
+    names.forEach(function (a: string) {
+    a = a + a
+    console.log(a)
+    });
+  
+
+
+
+
+
+
