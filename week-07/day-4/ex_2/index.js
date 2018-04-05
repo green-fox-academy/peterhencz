f230242e6e10cce90f5839a76ef75eb13c4e2e19
Peter Hencz <peter.hencz@gmail.com>
2018-04-05 23:21:20 +0200

    weatheeeeeer app is (almost) done

--- week-07/day-4/ex_2/index.js
+++ week-07/day-4/ex_2/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+app.use('/static', express.static('static'));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
