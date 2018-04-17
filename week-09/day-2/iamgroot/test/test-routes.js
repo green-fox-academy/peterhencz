95e86d582a908e9dafea1958c7b4656b4682e37f
Peter Hencz <peter.hencz@gmail.com>
2018-04-17 14:27:21 +0200

    yonda

--- week-09/day-2/iamgroot/test/test-routes.js
+++ week-09/day-2/iamgroot/test/test-routes.js
@@ -6,29 +6,59 @@ const app = require('../routes');
 
 test('groot endpoint', (t) => {
   request(app)
-    .get('/groot?message=')
-    .expect(200)
-    .expect('Content-Type', /json/)
-    .expect({
-      "received": "",
-      "translated": "I am Groot!"
-    })
-    .end((err,res) => {
-      t.error(err);
-      t.end();
-    });
+  .get('/groot?message=')
+  .expect(200)
+  .expect('Content-Type', /json/)
+  .expect({
+    "received": "",
+    "translated": "I am Groot!"
+  })
+  .end((err, res) => {
+    t.error(err);
+    t.end();
+  });
 });
 
 test('groot endpoint', (t) => {
   request(app)
-    .get('/groot')
-    .expect(400)
-    .expect('Content-Type', /json/)
-    .expect({
-      'error': 'I am Groot!',
-    })
-    .end((err,res) => {
-      t.error(err);
-      t.end();
-    });
+  .get('/groot')
+  .expect(400)
+  .expect('Content-Type', /json/)
+  .expect({
+    'error': 'I am Groot!',
+  })
+  .end((err,res) => {
+    t.error(err);
+    t.end();
+  });
+});
+
+test('yonda endpoint', (t) => {
+  request(app)
+  .get('/yonda?distance=100&time=10')
+  .expect(200)
+  .expect('Content-Type', /json/)
+  .expect({
+    distance: '100',
+    time: '10',
+    speed: '10'
+  })
+  .end((err, res) => {
+    t.error(err);
+    t.end();
+  });
+});
+
+test('yonda endpoint', (t) => {
+  request(app)
+  .get('/yonda')
+  .expect(400)
+  .expect('Content-Type', /json/)
+  .expect({
+    error: 'error'
+  })
+  .end((err, res) => {
+    t.error(err);
+    t.end();
+  });
 });
