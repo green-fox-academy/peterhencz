268086ea553e56c3c7330fbc1cf1f1f5ae115323
Peter Hencz <peter.hencz@gmail.com>
2018-04-17 14:27:02 +0200

    yonda

--- week-09/day-2/iamgroot/routes.js
+++ week-09/day-2/iamgroot/routes.js
@@ -15,22 +15,20 @@ app.get('/groot', (req, res) => {
   }
 });
 
+app.get('/yonda', (req, res) => {
+  if (req.query.distance && req.query.time !== undefined) {
+    res.json({
+      'distance': req.query.distance,
+      'time': req.query.time,
+      'speed': req.query.distance / req.query.time
+    })
+  }
+  else {
+    res.status(400).json({
+      error: 'error'
+    });
+  }
+});
 
 
 module.exports = app;
-
-app.get('/yonda', (req, res) => {
-  if (req.query.message !== undefined) {
-    res.json ({
-      "received": req.query.message,
-      "translated": "I am Groot!"
-    });
-  } else
-  {
-    res.json({
-      "distance": req.query.distance,
-      "time": req.query.time,
-      "speed": req.query.distance / req.query.time
-    })
-  }
-});
